d45e21fee09abf9fafe10abf674428815dddbc1c
Alejandro <smarketing.cuz@gmail.com>
2024-05-13 17:36:01 +0200

    remove envs

--- packages/site/app/api/auth/[...nextauth]/route.ts
+++ packages/site/app/api/auth/[...nextauth]/route.ts
@@ -5,8 +5,8 @@ const handler = NextAuth({
   providers: [
     GitHubProvider({
       name: "GitHub",
-      clientId: "Ov23lizDr9mWTm94h7mq",
-      clientSecret: "740d1cdba3dcc21687ad9b4a137e407cf8c1244d",
+      clientId: process.env.CLIENT_ID!,
+      clientSecret: process.env.CLIENT_SECRET!,
     }),
   ],
 });
